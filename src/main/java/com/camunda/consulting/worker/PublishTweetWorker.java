1fc742567e537025f5554b630b5d1ca5b20b1dee
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-07-27 17:30:42 +0200

    working now

--- src/main/java/com/camunda/consulting/worker/PublishTweetWorker.java
+++ src/main/java/com/camunda/consulting/worker/PublishTweetWorker.java
@@ -2,12 +2,14 @@ package com.camunda.consulting.worker;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.api.worker.JobHandler;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 
+@Component
 public class PublishTweetWorker implements JobHandler {
   
   private static final Logger LOG = LoggerFactory.getLogger(PublishTweetWorker.class);
