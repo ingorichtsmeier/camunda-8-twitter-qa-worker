127915bc33576ee0e244598d43d3d8c9ae082006
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-07-27 17:23:25 +0200

    minimal application, didn't run

---
+++ src/main/java/com/camunda/consulting/worker/PublishTweetWorker.java
@@ -0,0 +1,22 @@
+package com.camunda.consulting.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.client.api.worker.JobHandler;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+
+public class PublishTweetWorker implements JobHandler {
+  
+  private static final Logger LOG = LoggerFactory.getLogger(PublishTweetWorker.class);
+
+  @ZeebeWorker(type = "tweetPublishing")
+  public void handle(JobClient client, ActivatedJob job) throws Exception {
+    LOG.info("Publish something");
+    
+    client.newCompleteCommand(job).send().join();
+  }
+
+}
